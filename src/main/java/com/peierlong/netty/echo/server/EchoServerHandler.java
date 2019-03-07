04aba577051ffbc9f9f7affd9b94e71b1e85175b
peierlong <peielcn@gmail.com>
2019-03-07 15:36:23 +0800

    Netty EchoServer demo

---
+++ src/main/java/com/peierlong/netty/echo/server/EchoServerHandler.java
@@ -0,0 +1,37 @@
+package com.peierlong.netty.echo.server;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandler.Sharable;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.util.CharsetUtil;
+
+/**
+ * @author Peiel
+ * @version V1.0
+ * @date 2019-03-07
+ */
+@Sharable
+public class EchoServerHandler extends ChannelHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, java.lang.Object msg) {
+        ByteBuf in = (ByteBuf) msg;
+        System.out.println("Server received: " + in.toString(CharsetUtil.UTF_8));
+        ctx.write(in);
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) {
+        ctx.writeAndFlush(Unpooled.EMPTY_BUFFER).addListener(ChannelFutureListener.CLOSE);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+}
